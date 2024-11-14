437a04a402b81f9879aa972dacc0c2bd0e0681b7
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-11-14 21:33:31 +0530

    Add files via upload

---
+++ Day-2/Task-2/Dawn.py
@@ -0,0 +1,27 @@
+"""
+Author: Dawn K Vinod
+Date: 14-11-2024
+"""
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:"))
+#Addition
+addition=num1+num2
+print(f"Addition of the given two numbers: {addition}")
+#Subtraction
+subtraction=num1-num2
+print(f"Subtraction of the given two numbers: {subtraction}")
+#Multiplication
+product=num1*num2
+print(f"Product of the given two numbers: {product}")
+#Division
+division=num1/num2
+print(f"Division of the given two numbers: {division}")
+#Modulus
+remainder=num1%num2
+print(f"Remainder when {num1} is divided by {num2}: {remainder}")
+#Floor Division
+quotient=num1//num2
+print(f"Quotient when {num1} is divided by {num2}: {quotient}")
+#Exponential
+exponent=num1**num2
+print(f"{num1} raised to the power {num2}: {exponent}")
