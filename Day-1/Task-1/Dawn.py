f6f2e4c1e242f87b50d4f81cb0b446d9e6708cd5
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-11-14 18:47:59 +0530

    Add files via upload

---
+++ Day-1/Task-1/Dawn.py
@@ -0,0 +1,10 @@
+"""
+Author: Dawn K Vinod
+Date:14-11-2024
+"""
+
+#Assigning the string-"I Love Python" to a variable str1.
+str1="I Love Python"
+
+#Using print statement to display the given string.
+print(str1)
