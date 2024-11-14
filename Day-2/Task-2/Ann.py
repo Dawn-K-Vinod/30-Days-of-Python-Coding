3b22312682a5b39dc27fbd74e05d18141bd1c399
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-14 20:59:45 +0530

    Update Task-2

---
+++ Day-2/Task-2/Ann.py
@@ -0,0 +1,20 @@
+"""
+Author:Ann Mariya Tony
+Date : 14-11-2024
+"""
+num1=int(input("Enter the 1st number"))
+num2=int(input("Enter the 2nd number"))
+Addition = num1 + num2
+Subtraction = num1 - num2
+Multiplication = num1*num2
+Division = num1/num2
+Modulus = num1%num2
+Exponentiation = num1**num2
+Floordivision = num1//num2
+print("Sum:",Addition)
+print("Difference:",Subtraction)
+print("Product:",Multiplication)
+print("quotient:",Division)
+print("Modulus:",Modulus)
+print("Exponent:",Exponentiation)
+print("floor division:",Floordivision)
