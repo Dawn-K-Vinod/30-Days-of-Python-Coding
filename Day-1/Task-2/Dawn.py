a0f029683ea181a1ac864aede950d01db646cdcd
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-11-14 18:49:43 +0530

    Add files via upload

---
+++ Day-1/Task-2/Dawn.py
@@ -0,0 +1,10 @@
+"""
+Author: Dawn K Vinod
+Date: 14-11-2024
+"""
+
+#Assigning the string inputted by a user into the variable str1.
+str1=input("Enter a string:")
+
+#Using print statement to display the given string.
+print(str1)
