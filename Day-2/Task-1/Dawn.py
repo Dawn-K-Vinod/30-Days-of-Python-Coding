6fb9ea2ec57c52feef2a650a6b594b2e3ee236f8
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-11-14 18:56:56 +0530

    Add files via upload

---
+++ Day-2/Task-1/Dawn.py
@@ -0,0 +1,12 @@
+"""
+Author: Dawn K Vinod
+Date: 14-11-2024
+"""
+length = float(input("Enter the length of the rectangle:"))
+width = float(input("Enter the width of the rectangle:"))
+
+area = (length*width)
+perimeter = 2*(length+width)
+
+print(f"Area of the rectangle is: {area}")
+print(f"Perimeter of the rectangle is: {perimeter}")
