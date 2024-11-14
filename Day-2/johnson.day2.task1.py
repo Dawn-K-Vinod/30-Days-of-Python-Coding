8907c345870ec2f72344de32dc3385e728cff2d9
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-11-14 06:39:27 +0530

    Add files via upload

---
+++ Day-2/johnson.day2.task1.py
@@ -0,0 +1,10 @@
+"""
+AUTHOR: Johnson Siby
+Date:14-11-2024
+"""
+l=float(input("Enter your length : "))
+w=float(input("Enter your width : "))
+area = l*w
+perimeter=2*(l+w)
+print(f"The area of the rectangle is {area}. ")
+print(f"The perimeter of the rectangle is {perimeter}")
---
+++ Day-2/johnsonsiby.day2.task2.py
@@ -0,0 +1,20 @@
+"""
+AUTHOR : Johnson Siby
+Date : 14-11-2024
+"""
+num1=int(input("Enter your number 1 : "))
+num2=int(input("Enter your number 2 : "))
+sum = num1+num2
+print(f"sum = {sum}")
+difference= num1 - num2
+print(f"difference {difference}")
+product = num1*num2
+print(f"product = {product}")
+quotient=num1/num2
+print(f"quotient = {quotient}")
+modulus=num1%num2
+print(f"modulus = {modulus}")
+exponent = num1**num2
+exponent=(f"exponent = {exponent} ")
+floordivision=num1//num2
+print(f"floor = {floordivision}")
