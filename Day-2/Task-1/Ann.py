85b5761bbbcd05e4db14c379b22774bce7d43d53
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-14 20:05:47 +0530

    Update Task-1

---
+++ Day-2/Task-1/Ann.py
@@ -0,0 +1,10 @@
+"""
+Author:Ann Mariya Tony
+Date : 14-11-2024
+"""
+l = float(input("Enter the length:"))
+w = float(input("Enter the width:"))
+Area = l*w
+Perimeter= 2*(l+w)
+print("Area of the rectangle=",Area)
+print("Perimeter of the rectangle=",Perimeter)
