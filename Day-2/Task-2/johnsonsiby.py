3c464bc6ad5f40e0e3b9c4497a13e5455a5581a5
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-11-15 21:02:20 +0530

    johnsonsiby.py

--- Day-2/Task-2/johnsonsiby.py
+++ Day-2/Task-2/johnsonsiby.py
@@ -4,8 +4,8 @@ Date : 14-11-2024
 """
 num1=int(input("Enter your number 1 : "))
 num2=int(input("Enter your number 2 : "))
-sum = num1+num2
-print(f"sum = {sum}")
+sum1 = num1+num2
+print(f"sum = {sum1}")
 difference= num1 - num2
 print(f"difference {difference}")
 product = num1*num2
