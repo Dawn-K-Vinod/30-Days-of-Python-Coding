282acf93f3ff85beb52bcbb051350cf0470b5645
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-11-16 20:50:53 +0530

    Add files via upload

---
+++ Day-3/Dawn.py
@@ -0,0 +1,11 @@
+"""
+Author: Dawn K Vinod
+Date: 16-11-2024
+"""
+str1=input("Enter a word or sentence:")
+print(f"The first chartacter of the string: {str1[0]}")
+print(f"The first three characters of the string: {str1[:3]}")
+print(f"The last character of the string: {str1[-1]}")
+print(f"The last three characters of the string: {str1[-3:]}")
+print(f"Every second character in the string: {str1[::2]}")
+print(f"Reverse of the string: {str1[::-1]}")
