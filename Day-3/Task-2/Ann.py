3123a5f44a8fde1805c3be5fe8bab50e21465ec5
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-18 20:15:08 +0530

    Update Task-2

---
+++ Day-3/Task-2/Ann.py
@@ -0,0 +1,11 @@
+"""
+Author:Ann Mariya Tony
+Date:17-11-2024
+"""
+word=input("Enter a word or sentence:")
+print("First character:",word[0])
+print("First three characters:",word[:3])
+print("Last character:",word[-1:])
+print("Last three characters:",word[-3:])
+print("Every second character:",word[::2])
+print("Reversed string:",word[::-1])
