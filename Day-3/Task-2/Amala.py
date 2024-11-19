48f1fe245ce25f948d7e99b3f27134787f62939f
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-19 19:30:59 +0530

    Create Amala.py

---
+++ Day-3/Task-2/Amala.py
@@ -0,0 +1,11 @@
+'''
+Author:Amala Manoj
+Date:17-11-2024
+'''
+str=input("Enter your first sentence:")
+print("First character:",str[0])
+print("First three character:",str[0:3])
+print("Last character:",str[:-1])
+print("Last three character:",str[-3:])
+print("Every second character:",str[::2])
+print("Reversed string:",str[::-1])
