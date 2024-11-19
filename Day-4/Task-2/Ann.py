b35916ba58607707e42e03dac79a6ea7df7e0f63
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-19 20:20:12 +0530

    Update Task-2

---
+++ Day-4/Task-2/Ann.py
@@ -0,0 +1,18 @@
+"""
+Author:Ann Mariya Tony
+Date:19-11-2024
+"""
+greeting="Hello"
+name="Ann"
+full_greeting=greeting+" "+name +"!"
+print(full_greeting)
+message = "Welcome to Python"
+print(message[0:7])
+print(message[:-10])
+sentence = "python is awesome"
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.split())  
+print(sentence.replace("awesome", "great"))  
+quote = "He said, \"Python is amazing!\""
+print(quote)
