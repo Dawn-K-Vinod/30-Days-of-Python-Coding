54932f565b09fadb8169851152e916f64444745f
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-19 21:38:31 +0530

    Create Amala.py

---
+++ Day-4/Task-2/Amala.py
@@ -0,0 +1,18 @@
+'''
+Author:Amala Manoj
+Date:
+'''
+greeting="Hello"
+name=("Amala")
+full_greeting=greeting + " " + name + "!"
+print(full_greeting)
+message="Welcome to Python"
+print(message[0:7])
+print(message[:-10])
+sentence="Python is awesome"
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.split())
+print(sentence.replace("awesome", "great"))
+quote="He said,\"Python is amazing!\""
+print(quote)
