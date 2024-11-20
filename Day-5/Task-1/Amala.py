e52115c2a8bfbfa5582449472c7a534522b63f71
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-20 21:39:00 +0530

    Create Amala.py

---
+++ Day-5/Task-1/Amala.py
@@ -0,0 +1,12 @@
+'''
+Author:Amala Manoj
+Date:19-11-2024
+'''
+num=int(input("Enter a number for the multiplication table:"))
+for i in range (1,11):
+    result=num*i
+    print(num,"*",i,"=",result)
+string=input("Enter a string:")
+print("Characters in the string:",string)
+for i in string:
+    print(i)
