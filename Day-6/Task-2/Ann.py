bac273ac80399448a55498adb3d28b1117a3ac9c
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-21 23:12:27 +0530

    Update Task-2

---
+++ Day-6/Task-2/Ann.py
@@ -0,0 +1,12 @@
+"""
+Author:Ann Mariya Tony
+Date:21-11-2024
+"""
+str=input("Enter a sentence:")
+list=str.split()
+print("Original list of words:",list)
+print("First three words:",list[0:3])
+print("Last two words:",list[-2:])
+print("Reversed list of words:",list[::-1])
+
+         
