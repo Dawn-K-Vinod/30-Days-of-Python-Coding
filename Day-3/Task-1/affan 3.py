a7854798a644e66bc7c8422e9441ca1d42a5c414
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-21 22:07:08 +0530

    Add files via upload

---
+++ Day-3/Task-1/affan 3.py
@@ -0,0 +1,15 @@
+
+'''
+Author:affan muhammed
+Date:16-11-2024
+'''
+name=input("Enter your name:")
+age=int(input("How old are you?:"))
+height=float(input("What is your height in cm?:"))
+height_=height/100
+print ("Hello,name,!")
+print ("You are",age,"years old and",height,"cm tall")
+print ("That is",height,"metres tall.")
+print ("Data Type of name:",type(name))
+print ("Data Type of age:",type(age))
+print ("Data Type of converted height:",type(height))
