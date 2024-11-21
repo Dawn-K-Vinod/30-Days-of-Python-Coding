0890dd2e19886f6fe38d87aa58971ff4abd710d5
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-21 22:05:47 +0530

    Add files via upload

---
+++ Day-2/Task-1/affan 2.py
@@ -0,0 +1,11 @@
+
+'''
+Author:affan muhammed
+Date:14-11-2024
+'''
+length=float(input("Enter the length:"))
+width=float(input("Enter the width:"))
+area=length*width
+perimeter=2*(length+width)
+print(f"The area of the rectangle is :{area}")
+print(f"The perimeter of the rectangle is :{perimeter}")
