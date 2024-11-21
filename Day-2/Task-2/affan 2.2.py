e10bb8ea632d736c77638465c11c1134c0e6a9ee
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-21 22:06:31 +0530

    Add files via upload

---
+++ Day-2/Task-2/affan 2.2.py
@@ -0,0 +1,20 @@
+
+'''
+Author:affan muhammed
+Date:14-11-2024'''
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+result=(number1+number2)
+print("The sum of number1 and number2 is:",result)
+diff=(number1-number2)
+print("The difference is:",diff)
+pro=(number1*number2)
+print("The product of number1 and number2 is:",pro)
+div=(number1/number2)
+print("The division is:",div)
+modulus=(number1%number2)
+print("The modulus is:",modulus)
+exp=(number1**number2)
+print("The exponentiation is: ",exp)
+floor=(number1//number2)
+print("The floor division is:",floor)
