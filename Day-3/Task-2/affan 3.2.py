bf745c98282a248896cc1605c61f2f5478f3bef4
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-21 22:07:48 +0530

    Add files via upload

---
+++ Day-3/Task-2/affan 3.2.py
@@ -0,0 +1,12 @@
+
+'''
+Author:affan muhammed
+Date:17-11-2024
+'''
+str=input("Enter your first sentence:")
+print("First character:",str[0])
+print("First three character:",str[0:3])
+print("Last character:",str[:-1])
+print("Last three character:",str[-3:])
+print("Every second character:",str[::2])
+print("Reversed string:",str[::-1])
