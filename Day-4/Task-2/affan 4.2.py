64f77ff6a18adbdec420431b100906dcf9f2ff8e
affanmuhammed1234 <affanmuhammed2028@cs.sjcetpalai.ac.in>
2024-11-21 22:10:38 +0530

    Add files via upload

---
+++ Day-4/Task-2/affan 4.2.py
@@ -0,0 +1,19 @@
+
+'''
+Author:Affan muhammed
+Date:
+'''
+greeting="Hello"
+name=("affan")
+full_greeting=greeting + " " + name + "!"
+print(full_greeting)
+message="Welcome to Python"
+print(message[0:7])
+print(message[:-10])
+sentence="Python is awesome"
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.split())
+print(sentence.replace("awesome", "great"))
+quote="He said,\"Python is amazing!\""
+print(quote)
