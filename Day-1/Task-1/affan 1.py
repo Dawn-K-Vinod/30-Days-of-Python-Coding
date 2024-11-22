e6e1986580bce2216754fa7d3395f83635f0bd15
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2024-11-22 15:08:15 +0530

    Update affan 1.py

--- Day-1/Task-1/affan 1.py
+++ Day-1/Task-1/affan 1.py
@@ -1,2 +1,14 @@
 str1=("I Love Python.")
 print(str1)
+print("Increasing Triangle")
+n=int(input("Enter the no.of rows:"))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end="")
+    print(" ")
+print("Decreasing Triangle")
+d=int(input("enter the no.of rows"))
+for i in range(d):
+    for j in range(n-i):
+       print("*",end="")
+    print(" ")
