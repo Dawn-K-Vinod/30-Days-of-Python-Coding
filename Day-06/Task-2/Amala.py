57b8aa36783def99c106be5fa0c62f9a3e18d090
amalamanoj <amalamanoj2028@cs.sjcetpalai.ac.in>
2024-11-27 20:01:07 +0530

    Create Amala.py

---
+++ Day-06/Task-2/Amala.py
@@ -0,0 +1,10 @@
+'''
+Author:Amala Manoj
+Date:24-11-2024
+'''
+sen=str(input("Enter a sentence:"))
+list=sen.split()
+print("Original list of words:",list)
+print("First three words:",list[:3])
+print("Last two words:",list[4:])
+print("Reverse list of words:",list[::-1])
