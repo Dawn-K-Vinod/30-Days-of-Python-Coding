24ea78f3dc3014958876dd82b00c8eb7b849ccb5
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-11-28 21:45:41 +0530

    Add files via upload

---
+++ Day-03/Task-2/johnson.py
@@ -0,0 +1,11 @@
+"""
+AUTHOR: Johnson Siby
+Date:15-11-2024
+"""
+str1=input("Enter a word or sentence :")
+print("First charactor ",str1[0])
+print("First three characters:",str1[0:3])
+print("Last character:",str1[-1])
+print("Last three characters:", str1[-3:])
+print("Every second character: ",str1[::2])
+print("Reversed string: ",str1[::-1])
