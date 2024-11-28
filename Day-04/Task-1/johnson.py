fc013dee8f6a567a83f710d75083f8bfa725b04c
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-11-28 21:51:39 +0530

    Add files via upload

---
+++ Day-04/Task-1/johnson.py
@@ -0,0 +1,22 @@
+"""
+AUTHOR: Johnson Siby
+Date:16-11-2024
+"""
+
+first_side = float(input("Enter the length of the first side: "))
+second_side = float(input("Enter the length of the second side: "))
+third_side = float(input("Enter the length of the third side: "))
+
+if (first_side + second_side > third_side and
+        second_side + third_side > first_side and
+        first_side + third_side > second_side):
+
+
+    if first_side == second_side == third_side:
+        print("Equilateral")
+    elif first_side == second_side or second_side == third_side or first_side == third_side:
+        print("Isosceles")
+    else:
+        print("Scalene")
+else:
+    print("Not a triangle")
