70e2891999120df7fce784e5b026924f5f3c5fa1
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-11-29 19:26:21 +0530

    Add files via upload

---
+++ Day-04/Task-2/johnson.py
@@ -0,0 +1,22 @@
+"""
+AUTHOR: Johnson Siby
+Date:18-11-2024
+"""
+
+greetings="Hello"
+name="Bob"
+full_greetings=greetings+","+name
+print(full_greetings)
+
+message="Welcome to python"
+print(message[0:7])
+print(message[:-10])
+
+sentence="python is amazing"
+print(sentence.lower())
+print(sentence.upper())
+print(sentence.split())
+print(sentence.replace("amazing","great"))
+
+quote = "He said ,\"I am a student.\""
+print(quote)
