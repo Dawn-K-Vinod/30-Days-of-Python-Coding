deddcc879e6fd1ac4921390329d84634c160c390
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-11-29 20:32:56 +0530

    Add files via upload

---
+++ Day-05/Task-2/johnson.py
@@ -0,0 +1,17 @@
+"""
+AUTHOR: Johnson Siby
+Date:18-11-2024
+"""
+
+n1 = int(input("Enter a natural number upto which you want to find sum:"))
+n2=n1
+while True:
+    if n2<=0:
+        print("This is not a natural number")
+    else:
+        sum_n=0
+        while n2>0:
+            sum_n += n2
+            n2-=1
+        break
+print(f"The sum of natural numbers from 1 to {n1} is {sum_n}.")
