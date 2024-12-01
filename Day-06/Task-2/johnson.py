1687d5df7b46035a88ad180d06d30976f58ebff0
Johnsonsiby77 <johnsonsiby3@gmail.com>
2024-12-01 13:41:11 +0530

    Add files via upload

---
+++ Day-06/Task-2/johnson.py
@@ -0,0 +1,11 @@
+"""
+AUTHOR: Johnson Siby
+Date:1-12-2024
+"""
+
+s=input("Enter a sentence :")
+splited =s.split()
+print("Original list of words: ", splited)
+print("the first three words: ",splited[0:3])
+print("The last two words :",splited[-2:])
+print("Reversed list : ",splited[::-1])
